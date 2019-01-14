8cfd5cce670953f3cc239f1e0652dee5d928022c
Mohamadreza Mirali <smrm1363@gmail.com>
2019-01-14 14:06:35 +0330

    continue

--- src/main/java/com/mohammadrezaMirali/camundaCodeChallenge/CamundaCodeChallengeApplication.java
+++ src/main/java/com/mohammadrezaMirali/camundaCodeChallenge/CamundaCodeChallengeApplication.java
@@ -1,11 +1,15 @@
 package com.mohammadrezaMirali.camundaCodeChallenge;
 
+
+import com.mohammadrezaMirali.camundaCodeChallenge.model.BpmnModel;
 import com.mohammadrezaMirali.camundaCodeChallenge.model.BpmnService;
+
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.instance.FlowNode;
 import org.camunda.bpm.model.bpmn.instance.SequenceFlow;
 import org.camunda.bpm.model.bpmn.instance.StartEvent;
-import org.camunda.bpm.model.bpmn.instance.Task;
+import org.camunda.bpm.model.cmmn.instance.Task;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
 import org.camunda.bpm.model.xml.type.ModelElementType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +20,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.Collection;
+import java.util.*;
 
 @SpringBootApplication
 public class CamundaCodeChallengeApplication {
@@ -34,16 +38,61 @@ public class CamundaCodeChallengeApplication {
 		public void run(String... args) throws Exception {
 			// Put your logic here
 			System.out.println("Hiiiii ...");
-			InputStream inputStream = new ByteArrayInputStream(bpmnService.readBpmn().getBpmn20Xml().getBytes());
+			BpmnModel bpmnModel = bpmnService.readBpmn();
+			InputStream inputStream = new ByteArrayInputStream(bpmnModel.getBpmn20Xml().getBytes());
 			BpmnModelInstance modelInstance = Bpmn.readModelFromStream(inputStream);
-			StartEvent start = (StartEvent) modelInstance.getModelElementById("StartEvent_1");
-			ModelElementType taskType = modelInstance.getModel().getType(Task.class);
-			Collection<ModelElementInstance> taskInstances = modelInstance.getModelElementsByType(taskType);
-			System.out.println(start.getName());
-			SequenceFlow sequenceFlow = (SequenceFlow) modelInstance.getModelElementById("approveInvoice");
+
+			// find all elements of the type task
+			ModelElementType sequenceFlowType = modelInstance.getModel().getType(SequenceFlow.class);
+			Collection<ModelElementInstance> sequenceFlowInstances = modelInstance.getModelElementsByType(sequenceFlowType);
+			sequenceFlowInstances.forEach(modelElementInstance ->
+			{
+				SequenceFlow sequenceFlow1 = (SequenceFlow) modelElementInstance;
+				System.out.println(sequenceFlow1.getSource().getElementType().getTypeName());
+			});
+
+			Optional<SequenceFlow> source = sequenceFlowInstances.stream().filter(modelElementInstance ->
+			{
+				SequenceFlow sequenceFlow1 = (SequenceFlow) modelElementInstance;
+				return sequenceFlow1.getSource().getId().equals(args[0]);
+			}).findFirst().map(modelElementInstance -> ((SequenceFlow)modelElementInstance));
+
+			Optional<SequenceFlow> dest = sequenceFlowInstances.stream().filter(modelElementInstance ->
+			{
+				SequenceFlow sequenceFlow1 = (SequenceFlow) modelElementInstance;
+				return sequenceFlow1.getTarget().getId().equals(args[1]);
+			}).findFirst().map(modelElementInstance -> ((SequenceFlow)modelElementInstance));
+
+			List<String> path = new ArrayList<>();
+			path.add(source.get().getSource().getId());
+			path.add(source.get().getTarget().getId());
+			bpmnModel.printPath(source.get(),dest.get(),path,new HashSet<>());
+//			printPath(source.get(),dest.get());
+
+//			StartEvent start = (StartEvent) modelInstance.getModelElementById("approveInvoice");
+			ModelElementInstance modelElementInstance =  modelInstance.getModelElementById("approveInvoice");
+			SequenceFlow sequenceFlow = (SequenceFlow) modelInstance.getModelElementById("sequenceFlow_180");
+			// get the source and target element
+//			FlowNode source = sequenceFlow.getSource();
+			FlowNode target = sequenceFlow.getTarget();
+
+// get all outgoing sequence flows of the source
+//			Collection<SequenceFlow> outgoing = source.getOutgoing();
+//			assert(outgoing.contains(sequenceFlow));
 			System.out.println("Hiiiii ...2");
 		}
 
+	}
+
+	private void printPath(FlowNode source,FlowNode dest, List<String> flowNodeIdList)
+	{
+
+		while (source.getOutgoing() != null || source.getOutgoing().size()>0)
+		{
+
+		}
+		source.getOutgoing().forEach(sequenceFlow -> System.out.println(sequenceFlow.getTarget().getId()));
+		System.out.println(source.getOutgoing());
 	}
 
 }
--- src/main/java/com/mohammadrezaMirali/camundaCodeChallenge/model/BpmnModel.java
+++ src/main/java/com/mohammadrezaMirali/camundaCodeChallenge/model/BpmnModel.java
@@ -1,6 +1,11 @@
 package com.mohammadrezaMirali.camundaCodeChallenge.model;
 
+import org.camunda.bpm.model.bpmn.instance.FlowNode;
+import org.camunda.bpm.model.bpmn.instance.SequenceFlow;
+
 import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by mmirali on 13/01/2019.
@@ -23,5 +28,32 @@ public class BpmnModel implements Serializable{
 
     public void setBpmn20Xml(String bpmn20Xml) {
         this.bpmn20Xml = bpmn20Xml;
+    }
+
+
+    public void printPath(SequenceFlow source, SequenceFlow dest, List<String> path, Set<String> visitedSet)
+    {
+
+        if(source.getTarget().getId().equals(dest.getTarget().getId()))
+        {
+            System.out.println(path);
+            return;
+        }
+        visitedSet.add(source.getTarget().getId());
+        source.getTarget().getOutgoing().forEach(sequenceFlow ->
+        {
+            if(!visitedSet.contains(sequenceFlow.getTarget().getId()))
+            {
+                path.add(sequenceFlow.getTarget().getId());
+                printPath(sequenceFlow,dest,path,visitedSet);
+                path.remove(sequenceFlow.getTarget().getId());
+            }
+        });
+
+        visitedSet.remove(source.getTarget().getId());
+
+
+//        source.getOutgoing().forEach(sequenceFlow -> System.out.println(sequenceFlow.getTarget().getId()));
+//        System.out.println(source.getOutgoing());
     }
 }
