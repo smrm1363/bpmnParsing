f75f7cab255cddff1d229c16aecededdc8120054
Mohamadreza Mirali <smrm1363@gmail.com>
2019-01-16 16:14:51 +0330

    Finish

--- src/main/java/com/mohammadrezaMirali/camundaCodeChallenge/CamundaCodeChallengeApplication.java
+++ src/main/java/com/mohammadrezaMirali/camundaCodeChallenge/CamundaCodeChallengeApplication.java
@@ -37,63 +37,18 @@ public class CamundaCodeChallengeApplication {
 		@Override
 		public void run(String... args) throws Exception {
 			// Put your logic here
-
-			BpmnModel bpmnModel = bpmnService.readBpmn();
-			InputStream inputStream = new ByteArrayInputStream(bpmnModel.getBpmn20Xml().getBytes());
-			BpmnModelInstance modelInstance = Bpmn.readModelFromStream(inputStream);
-
-			// find all elements of the type task
-			ModelElementType sequenceFlowType = modelInstance.getModel().getType(SequenceFlow.class);
-			Collection<ModelElementInstance> sequenceFlowInstances = modelInstance.getModelElementsByType(sequenceFlowType);
-			sequenceFlowInstances.forEach(modelElementInstance ->
+			try {
+				bpmnService.printBpmnBetweenTwoNode(args[0], args[1]);
+			}
+			catch (ArrayIndexOutOfBoundsException e)
 			{
-				SequenceFlow sequenceFlow1 = (SequenceFlow) modelElementInstance;
-				System.out.println(sequenceFlow1.getSource().getElementType().getTypeName());
-			});
+				System.out.println("For running this program, you should specify two parameters as source and dest in BPMN");
+			}
 
-			Optional<SequenceFlow> source = sequenceFlowInstances.stream().filter(modelElementInstance ->
-			{
-				SequenceFlow sequenceFlow1 = (SequenceFlow) modelElementInstance;
-				return sequenceFlow1.getSource().getId().equals(args[0]);
-			}).findFirst().map(modelElementInstance -> ((SequenceFlow)modelElementInstance));
-
-			Optional<SequenceFlow> dest = sequenceFlowInstances.stream().filter(modelElementInstance ->
-			{
-				SequenceFlow sequenceFlow1 = (SequenceFlow) modelElementInstance;
-				return sequenceFlow1.getTarget().getId().equals(args[1]);
-			}).findFirst().map(modelElementInstance -> ((SequenceFlow)modelElementInstance));
-
-			List<String> path = new ArrayList<>();
-			path.add(source.get().getSource().getId());
-			path.add(source.get().getTarget().getId());
-			bpmnModel.printPath(source.get(),dest.get(),path,new HashSet<>());
-//			printPath(source.get(),dest.get());
-
-//			StartEvent start = (StartEvent) modelInstance.getModelElementById("approveInvoice");
-			ModelElementInstance modelElementInstance =  modelInstance.getModelElementById("approveInvoice");
-			SequenceFlow sequenceFlow = (SequenceFlow) modelInstance.getModelElementById("sequenceFlow_180");
-			// get the source and target element
-//			FlowNode source = sequenceFlow.getSource();
-			FlowNode target = sequenceFlow.getTarget();
-
-// get all outgoing sequence flows of the source
-//			Collection<SequenceFlow> outgoing = source.getOutgoing();
-//			assert(outgoing.contains(sequenceFlow));
-			System.out.println("Hiiiii ...2");
 		}
 
 	}
 
-	private void printPath(FlowNode source,FlowNode dest, List<String> flowNodeIdList)
-	{
-
-		while (source.getOutgoing() != null || source.getOutgoing().size()>0)
-		{
-
-		}
-		source.getOutgoing().forEach(sequenceFlow -> System.out.println(sequenceFlow.getTarget().getId()));
-		System.out.println(source.getOutgoing());
-	}
 
 }
 
--- src/main/java/com/mohammadrezaMirali/camundaCodeChallenge/model/BpmnService.java
+++ src/main/java/com/mohammadrezaMirali/camundaCodeChallenge/model/BpmnService.java
@@ -1,32 +1,82 @@
 package com.mohammadrezaMirali.camundaCodeChallenge.model;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.instance.FlowNode;
+import org.camunda.bpm.model.bpmn.instance.SequenceFlow;
+import org.camunda.bpm.model.xml.instance.ModelElementInstance;
+import org.camunda.bpm.model.xml.type.ModelElementType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
+import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.*;
 
 /**
  * Created by mmirali on 13/01/2019.
  */
 @Service
 public class BpmnService {
+    @Value("${bpmn-file-url}")
+    private String bpmnUrlPath;
     public BpmnModel readBpmn()
     {
         Client client = ClientBuilder.newBuilder().newClient().register(JacksonJsonProvider.class);
-        WebTarget target = client.target("https://elxkoom6p4.execute-api.eu-central-1.amazonaws.com/prod/engine-rest/process-definition/key/invoice/xml");
-
+        WebTarget target = client.target(bpmnUrlPath);
         Invocation.Builder builder = target.request();
+        try {
         Response response = builder.get();
+        }
+        catch (ProcessingException e)
+        {
+            System.out.println(e.getMessage());
+        }
         BpmnModel bpmnModel = builder.get(BpmnModel.class);
         return bpmnModel;
     }
 
-    public void printBpmnBetweenTwoNode(String source,String dest)
+    public void printBpmnBetweenTwoNode( String sourceNode, String destNode)
     {
+        BpmnModel bpmnModel = readBpmn();
+        InputStream inputStream = new ByteArrayInputStream(bpmnModel.getBpmn20Xml().getBytes());
+        BpmnModelInstance modelInstance = Bpmn.readModelFromStream(inputStream);
+
+        // find all elements of the type task
+        ModelElementType sequenceFlowType = modelInstance.getModel().getType(SequenceFlow.class);
+        Collection<ModelElementInstance> sequenceFlowInstances = modelInstance.getModelElementsByType(sequenceFlowType);
+
+        Optional<SequenceFlow> source = sequenceFlowInstances.stream().filter(modelElementInstance ->
+        {
+            SequenceFlow sequenceFlow1 = (SequenceFlow) modelElementInstance;
+            return sequenceFlow1.getSource().getId().equals(sourceNode);
+        }).findFirst().map(modelElementInstance -> ((SequenceFlow)modelElementInstance));
+
+        Optional<SequenceFlow> dest = sequenceFlowInstances.stream().filter(modelElementInstance ->
+        {
+            SequenceFlow sequenceFlow1 = (SequenceFlow) modelElementInstance;
+            return sequenceFlow1.getTarget().getId().equals(destNode);
+        }).findFirst().map(modelElementInstance -> ((SequenceFlow)modelElementInstance));
+
+        List<String> path = new ArrayList<>();
+        try {
+            path.add(source.get().getSource().getId());
+            path.add(source.get().getTarget().getId());
+            System.out.println("The path from approveInvoice to invoiceProcessed is:");
+            bpmnModel.printPath(source.get(), dest.get(), path, new HashSet<>());
+        }
+        catch (NoSuchElementException e)
+        {
+            System.out.println("One of the source or destination element's ID is not correct, means not found in the BPMN");
+        }
 
     }
 }
